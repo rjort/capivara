b1a7956ba308fd7f1c21196f57256e672758d161
Reuter Junior <reuter.regis@compasso.com.br>
2024-04-01 19:14:29 -0300

    add: new require 'validate_arg_value'

--- lib/capivara.rb
+++ lib/capivara.rb
@@ -7,6 +7,7 @@ require 'fileutils'
 require 'commands/handlers/generate_handler'
 require 'commands/handlers/create_handler'
 require 'commands/handlers/command_data'
+require 'commands/handlers/validate_arg_value'
 
 require 'commands/generate'
 require 'commands/create'
