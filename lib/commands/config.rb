19ea20a7206c71cc08f4fd3e4dd03850e2cf99f6
Reuter Junior <reuter.regis@compasso.com.br>
2024-04-01 19:18:01 -0300

    fix: Improved STDERR and STDOUT handling, removed arbitrary exit codes.

--- lib/commands/config.rb
+++ lib/commands/config.rb
@@ -1,6 +1,6 @@
 module Commands
   class Config
-    include Commands::Handle::CommandData
+    include Commands::Handlers::CommandData
 
     def initialize
       @data ||= data_config
@@ -27,18 +27,17 @@ module Commands
     def create_capivara_json(json_path)
       if !File.exist?(json_path)
         File.open(json_path, 'w') { |file| file.write(JSON.pretty_generate(@data[:json_data]))}
-        puts "Created: #{File.basename(json_path)}".colorize(:green)
+        STDOUT.puts "Created: #{File.basename(json_path)}".colorize(:green)
       elsif File.exist?(json_path)
-        puts "The file already exists. Do you want to overwrite it?( [y]es/[n]o".colorize(:yellow)
+        STDERR.puts "The file already exists. Do you want to overwrite it?( [y]es/[n]o"
         answer = $stdin.gets.chomp.downcase
         if answer == 'y' || answer == 'yes'
           json = File.open(json_path, 'w') { |file| file.write(JSON.pretty_generate(@data[:json_data])) }
-          puts "Overwritten: #{File.basename(json_path)}".colorize(:green)
+          STDOUT.puts "Overwritten: #{File.basename(json_path)}".colorize(:green)
         else
-          exit -64
+          STDERR.puts "Config file #{File.basename(json_path)} not created".colorize(:yellow)
         end
       else
-        STDERR.puts "Config file #{File.basename(json_path)} not created".colorize(:red)
         exit -1
       end
     end
