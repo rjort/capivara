6003c8b2a60a3851e4dc02dfad4273427547a18c
Reuter Junior <reuter_junior@hotmail.com>
2024-05-13 12:26:14 -0300

    refactor: new data for config command

--- lib/commands/handlers/command_data.rb
+++ lib/commands/handlers/command_data.rb
@@ -47,14 +47,16 @@ module Commands
       end
 
       def data_config
-        @project_path = Dir.pwd
+        @project_root_path = Dir.pwd
         @project_name = File.basename(Dir.pwd)
-        @json_path    = "#{@project_path}/capivara.json"
+        @features_path = File.join(@project_root_path, 'features')
+        @json_path    = File.join(@project_root_path, 'capivara.json')
 
         {
-          project_path: @project_path,
           project_name: @project_name,
-          json_path: @json_path,
+          project_root_path: @project_root_path,
+          features_path: @features_path,
+          json_path: @json_path
         }
       end
 
